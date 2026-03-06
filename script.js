173876afc298dc119c0b9b250fa3c84fcbaac5e0
simonegreco5 <simoxey.greco@gmail.com>
2026-03-06 19:23:49 +0100

    start write first part of funcition

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let getName = prompt('type a word')
 // creiamo una funzione per capire se la parola
 // inserita è un palindroma.
 
-function isPalindroma() {
+function isPalindroma(word) {
+    const reverseWord = []
+    for (let i = word.length - 1; i >= 0; i--)
+        reverseWord.push(word[i])
     
+
 }
