e37d34fb937ad134cbae9a03901a3172dd253ace
simonegreco5 <simoxey.greco@gmail.com>
2026-03-06 18:54:21 +0100

    add prompt (for type a word) in ex. 'palindroma'

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('js-paliedispari')
+
+// esercizio 1 palindroma
+
+// chiediamo all'utente di inserire una parola.
+let getName = prompt('type a word')
+
+// creiamo una funzione per capire se la parola
+// inserita è un palindroma.
+
+function isPalindroma() {
+    
+}
